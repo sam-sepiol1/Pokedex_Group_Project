3b98df1bcc845182a21ad315698a3950fff06749
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2024-12-24 01:25:07 +0100

    handle sass deprecations error and add electric type scss

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sass from 'sass' // Importer le module sass
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass, // Utiliser l'API moderne de Dart Sass
+      },
+    },
+  },
+
   server: {
     hmr: true, // Explicitly enable HMR
-    port:5173,
+    port: 5173,
     watch: {
       usePolling: true
     },
   },
 });
-
